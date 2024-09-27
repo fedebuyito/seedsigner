ef6e44879ce989ad9e8491839b965a18aba13627
fedebuyito <126606287+fedebuyito@users.noreply.github.com>
2024-09-27 12:11:07 -0300

    Revert "Update tools_screens.py"
    This reverts commit e8b0e35b27bedbbab324a82d3d61d5be350c3fa7.

--- src/seedsigner/gui/screens/tools_screens.py
+++ src/seedsigner/gui/screens/tools_screens.py
@@ -129,7 +129,6 @@ class ToolsDiceEntropyEntryScreen(KeyboardScreen):
     def __post_init__(self):
         # Override values set by the parent class
         self.title = f"Dice Roll 1/{self.return_after_n_chars}"
-        self.custom_additional_keys = [Keyboard.KEY_BACKSPACE]
 
         # Specify the keys in the keyboard
         self.rows = 3
@@ -188,7 +187,6 @@ class ToolsCoinFlipEntryScreen(KeyboardScreen):
     def __post_init__(self):
         # Override values set by the parent class
         self.title = f"Coin Flip 1/{self.return_after_n_chars}"
-        self.custom_additional_keys = [Keyboard.KEY_BACKSPACE_2]
 
         # Specify the keys in the keyboard
         self.rows = 1
